f6f59600b04deecae1456fbda09d2fe860b7349d
usamariaz2 <osamarajpoot928@gmail.com>
2024-01-05 22:55:24 +0500

    mongodb update

---
+++ backend/db/db.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+dotenv.config()
+
+const connnectDB = async () => {
+	const uri =
+		"mongodb+srv://eduConnect:EKUGaD98G2eV8Djy@educonnect.zwczviv.mongodb.net/?retryWrites=true&w=majority"
+	const connectionParams = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+	mongoose
+		// .connect("mongodb://127.0.0.1:27017/vidmeti")
+		.connect(uri)
+		.then(() => console.log("Mongodb is connected"))
+		.catch((error) => console.log("Mongodb connection error", error))
+	// Handle unhandled promise rejections
+	process.on("unhandledRejection", (reason, promise) => {
+		console.error("Unhandled Rejection at:", promise, "reason:", reason)
+		// You can add custom handling logic here if needed.
+	})
+
+	// Handle unhandled exceptions (optional)
+	process.on("uncaughtException", (error) => {
+		console.error("Uncaught Exception:", error)
+		// You can add custom handling logic here if needed.
+	})
+}
+// export default connnectDB
+exports.connnectDB = connnectDB
--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,35 @@
 const express = require("express")
 const cors = require("cors")
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const bodyParser = require("body-parser")
 const app = express()
 const Routes = require("./routes/route.js")
-
+const db = require("./db/db.js")
 const PORT = process.env.PORT || 5000
+// Osama Update
+db.connnectDB()
+//
+dotenv.config()
 
-dotenv.config();
+app.use(bodyParser.json({ limit: "10mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }))
 
-app.use(bodyParser.json({ limit: '10mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
-
-app.use(express.json({ limit: '10mb' }))
+app.use(express.json({ limit: "10mb" }))
 app.use(cors())
 
-mongoose
-    .connect('mongodb://127.0.0.1:27017/database', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+// mongoose
+// 	.connect("mongodb://127.0.0.1:27017/database", {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 	})
+// 	.then(console.log("Connected to MongoDB"))
+// 	.catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 
-app.use('/', Routes);
-
+app.use("/", Routes)
+app.get("/", async (req, res) => {
+	res.json("Welcome to website")
+})
 app.listen(PORT, () => {
-    console.log(`Server started at port no. ${PORT}`)
+	console.log(`Server started at port no. ${PORT}`)
 })
