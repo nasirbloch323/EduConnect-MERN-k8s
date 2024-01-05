cfc086dc066062d9a4bca2320a7531e54413f67e
Musharaf Ali <musharafali494@gmail.com>
2024-01-05 13:08:44 +0500

    pages create

---
+++ frontend/src/pages/FrontUIPages/ContactPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <>
+          
+        </>
+    );
+};
+
+
+export default ContactPage;
---
+++ frontend/src/pages/FrontUIPages/MemberShipPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MemberShipPage = () => {
+    return (
+        <>
+          
+        </>
+    );
+};
+
+
+export default MemberShipPage;
