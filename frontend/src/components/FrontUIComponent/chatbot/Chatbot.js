ecc0d5008fb7aaec392d211b47db3bdf94b48126
Musharaf Ali <musharafali494@gmail.com>
2024-03-07 12:58:26 +0500

    add chatbot

---
+++ frontend/src/components/FrontUIComponent/chatbot/Chatbot.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./chatbot.css";
+
+function Chatbot() {
+
+
+  return (
+    <div className="chatbot">
+     <iframe	className="bg-[#EA4C89]" src="https://app.fastbots.ai/embed/cltgrdpm101bjs9b3luj9znnf"></iframe>
+    </div>
+  );
+}
+
+export default Chatbot;
