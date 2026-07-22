1307db2fe64b67cd97336bce6b6ad43cc77e5d29
Ubuntu <ubuntu@ip-172-31-84-189.ec2.internal>
2026-07-22 18:19:09 +0000

    updated

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,6 @@ const app = express()
 const Routes = require("./routes/route.js")
 const PORT = process.env.PORT || 5000
 
-// Ann
 const annRoute = require("./routes/products.js")
 const eventRoute = require("./routes/events.js")
 const researchRoute = require("./routes/research.js")
@@ -17,39 +16,34 @@ app.use(bodyParser.json({ limit: "10mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }))
 
 app.use(express.json({ limit: "10mb" }))
-app.use(cors())
 
 app.use("/files", express.static("files"))
 
-app.use(
-	cors({
-		origin: ["https://edu-connect1.vercel.app", "http://localhost:3000"],
-		methods: ["POST", "GET"],
-		credentials: true,
-	})
-)
+app.use(cors({
+    origin: "*",
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+}))
+
 app.use(express.json())
 
-const uri =
-	"mongodb://educonnect:educonnect123@ac-rd6itgi-shard-00-00.b8ejcje.mongodb.net:27017,ac-rd6itgi-shard-00-01.b8ejcje.mongodb.net:27017,ac-rd6itgi-shard-00-02.b8ejcje.mongodb.net:27017/educonnect?ssl=true&replicaSet=atlas-tnocmg-shard-0&authSource=admin&retryWrites=true&w=majority"
-
 mongoose
-	.connect(process.env.MONGODB_URL || uri, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(console.log("Connected to MongoDB"))
-	.catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+    .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
 
-// Ann
 app.use("/products", annRoute)
 app.use("/events", eventRoute)
 app.use("/researchs", researchRoute)
-//
 app.use("/", Routes)
+
 app.get("/", async (req, res) => {
-	res.json("Welcome to website")
+    res.json("Welcome to website")
 })
+
 app.listen(PORT, () => {
-	console.log(`Server started at port no. ${PORT}`)
+    console.log(`Server started at port no. ${PORT}`)
 })
